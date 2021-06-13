ec90afe74accd0aad935b0f1023afd1b080eae19
serenity4 <cedric.bel@hotmail.fr>
2021-06-13 12:42:27 +0200

    Allow arbitrary expressions in @record

--- src/gpu/command.jl
+++ src/gpu/command.jl
@@ -1,9 +1,19 @@
+walk(ex::Expr, inner, outer) = outer(Expr(ex.head, map(inner, ex.args)...))
+walk(ex, inner, outer) = outer(ex)
+
+postwalk(f, ex) = walk(ex, x -> postwalk(f, x), f)
+prewalk(f, ex) = walk(f(ex), x -> prewalk(f, x), identity)
+
 """
     @record command_buffer [create_info] commands
 
 Convenience macro for recording a sequence of API commands into a command buffer `cbuff`.
-All API commands have `cbuff` inserted as their first argument, and are wrapped inside
+All calls to API commands have `cbuff` inserted as their first argument, and are wrapped inside
 `begin_command_buffer(cbuff, info)` and `end_command_buffer(cbuff)`.
+
+!!! warning
+    An expression is assumed to be an API command if it begins with `cmd_`.
+    Make sure that all functions that you call satisfy this assumption.
 
 The two-argument version of this macro simply passes in a default `CommandBufferBeginInfo()`.
 """
@@ -13,11 +23,13 @@ macro record(cbuff, info, cmds)
         _ => [cmds]
     end
     _cbuff, _info = esc(cbuff), esc(info)
-    api_calls = map(filter(x -> !isa(x, LineNumberNode), cmds)) do cmd
-        @match cmd begin
-            :($f($(args...))) => :($f($_cbuff, $(esc.(args)...)))
-            :($f($(args...); $(kwargs...))) => :($f($_cbuff, $(esc.(args)...); $(esc.(kwargs)...)))
-            _ => error("Expected API call, got $cmd")
+    api_calls = map(cmds) do cmd
+        postwalk(cmd) do ex
+            @match ex begin
+                :($f($(args...))) => startswith(string(f), "cmd_") ? :($(esc(f))($_cbuff, $(esc.(args)...))) : ex
+                :($f($(args...); $(kwargs...))) => startswith(string(f), "cmd_") ? :($(esc(f))($_cbuff, $(esc.(args)...); $(esc.(kwargs)...))) : ex
+                _ => ex
+            end
         end
     end
     esc(quote
