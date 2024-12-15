9a2d81e6db829184081d81728f43eb2dd6c3524a
serenity4 <cedric.bel@hotmail.fr>
2024-12-15 18:02:13 +0100

    Update to latest Lava frame cycle integration changes

--- src/renderer.jl
+++ src/renderer.jl
@@ -31,12 +31,12 @@ mutable struct Renderer
   task::Task
   function Renderer(window::Window; release = is_release())
     instance, device = Lava.init(; debug = !release, with_validation = !release, instance_extensions = ["VK_KHR_xcb_surface"], device_specific_features = [:sample_rate_shading])
-    color = color_attachment(device, window)
+    color = new_color_attachment(device, window)
     new(instance, device, FrameCycle(device, Surface(instance, window); n = 2), color, ExecutionState[], ProgramCache(device), FrameDiagnostics())
   end
 end
 
-color_attachment(device::Device, window::Window) = attachment_resource(device, nothing; format = RGBA{Float16}, dims = collect(Int64, extent(window)), usage_flags = Vk.IMAGE_USAGE_TRANSFER_SRC_BIT | Vk.IMAGE_USAGE_COLOR_ATTACHMENT_BIT, samples = 8, name = :color)
+new_color_attachment(device::Device, window::Window) = attachment_resource(device, nothing; format = RGBA{Float16}, dims = collect(Int64, extent(window)), usage_flags = Vk.IMAGE_USAGE_TRANSFER_SRC_BIT | Vk.IMAGE_USAGE_COLOR_ATTACHMENT_BIT, samples = 8, name = :color)
 
 function start(renderer::Renderer)
   options = SpawnOptions(start_threadid = RENDERER_THREADID, disallow_task_migration = true, execution_mode = LoopExecution(0.005))
@@ -44,15 +44,14 @@ function start(renderer::Renderer)
 end
 
 function Lava.render(app, rdr::Renderer)
-  state = cycle!(rdr.frame_cycle) do image
+  state = cycle!(rdr.frame_cycle) do frame
     if collect(Int, extent(app.window)) ≠ dimensions(rdr.color.attachment)
-      rdr.color = color_attachment(rdr.device, app.window)
+      rdr.color = new_color_attachment(rdr.device, app.window)
     end
-    (; color) = rdr
-    ret = tryfetch(execute(() -> frame_nodes(color), task_owner()))
-    iserror(ret) && shutdown_scheduled() && return draw_and_prepare_for_presentation(rdr.device, RenderNode[], color, image)
+    ret = tryfetch(execute(() -> frame_nodes(rdr.color), task_owner()))
+    iserror(ret) && shutdown_scheduled() && return nothing
     nodes = unwrap(ret)::Vector{RenderNode}
-    draw_and_prepare_for_presentation(rdr.device, nodes, color, image)
+    draw_and_prepare_for_presentation(rdr.device, nodes, rdr.color, frame)
   end
   !isnothing(state) && push!(rdr.pending, state)
   filter!(exec -> !wait(exec, 0), rdr.pending)
